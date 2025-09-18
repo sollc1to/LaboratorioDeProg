3aea129629c020f34d2aa149b9df6d10299d21d0
Toewqr <teoarandalautaro@gmail.com>
2025-09-17 21:43:33 -0300

    metricas en loop

--- accordM.js
+++ accordM.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const label =element.querySelector('label');
         label.addEventListener('click',function(){
             element.classList.toggle('active'); 
-            /* Esta parte hace que se desactiven los demas si se clickea sobre un boton
+            /* Esta parte hace que se desactiven los demas si se clickea sobre un boton*/
             accordItems.forEach(otherelement =>{ 
                 if(otherelement!==element && otherelement.classList.contains('active')){
                     otherelement.classList.remove('active');
                 }
-            } ) }*/
+            } ) 
         })
     });
     
